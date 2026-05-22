fc55589c25c5ae812f236a6d66d18c8cdf469e79
Mateus Pilz <pilzmateus06@gmail.com>
2026-05-22 17:25:12 -0300

    Primeiro commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,6 @@
+<template>
+  <main class="conteudo-inicial">
+    <h1>Bem-vindo à Página Inicial em Vue!</h1>
+    <p>Este site agora roda utilizando rotas dinâmicas e reatividade de ponta.</p>
+  </main>
+</template>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,46 @@
+<script setup>
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+const email = ref('')
+const senha = ref('')
+const mensagemText = ref('')
+const mensagemCor = ref('')
+
+const realizarLogin = () => {
+  if (email.value === "admin@teste.com" && senha.value === "1234") {
+    mensagemCor.value = "green"
+    mensagemText.value = "Login realizado com sucesso! Redirecionando..."
+    
+    setTimeout(() => {
+      router.push('/') // Redireciona o usuário para a Home usando o Vue Router
+    }, 1500)
+  } else {
+    mensagemCor.value = "red"
+    mensagemText.value = "E-mail ou senha incorretos. Tente novamente."
+  }
+}
+</script>
+
+<template>
+  <main class="conteudo">
+    <div class="login-card">
+      <h2>Área de Login</h2>
+      <form @submit.prevent="realizarLogin">
+        <div class="input-group">
+          <label>E-mail</label>
+          <input type="email" v-model="email" placeholder="Digite seu e-mail" required>
+        </div>
+        
+        <div class="input-group">
+          <label>Senha</label>
+          <input type="password" v-model="senha" placeholder="Digite sua senha" required>
+        </div>
+
+        <button type="submit" class="btn-login">Entrar</button>
+      </form>
+      <p class="mensagem" :style="{ color: mensagemCor }">{{ mensagemText }}</p>
+    </div>
+  </main>
+</template>
